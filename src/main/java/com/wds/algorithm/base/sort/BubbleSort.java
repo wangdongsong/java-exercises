0039388ecf6ddba6b8fa2a00df6778212880c6b0
wangdongsong <wangdongsong1229@163.com>
2016-10-08 07:41:18 +0800

    提交冒泡排序、桶排序、快速排序代码

--- src/main/java/com/wds/algorithm/base/sort/BubbleSort.java
+++ src/main/java/com/wds/algorithm/base/sort/BubbleSort.java
@@ -1,7 +1,61 @@
 package com.wds.algorithm.base.sort;
 
 /**
+ * 冒泡排序
  * Created by wangdongsong on 2016/10/7.
  */
 public class BubbleSort {
+
+    private int[] arrays;
+
+    public BubbleSort(int[] arrays) {
+        this.arrays = arrays;
+    }
+
+    /**
+     * 从小到大
+     */
+    public void sortASC() {
+        int temp = 0;
+        if (arrays != null && arrays.length > 0) {
+            for (int i = 1; i < arrays.length; i++) {
+                for (int j = 0; j < arrays.length - i; j++) {
+                    if (arrays[j] > arrays[j + 1]) {
+                        temp = arrays[j];
+                        arrays[j] = arrays[j + 1];
+                        arrays[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    public void sortDESC() {
+        int temp = 0;
+        if (arrays != null && arrays.length > 0) {
+            for (int i = 1; i < arrays.length; i++) {
+                for (int j = 0; j < arrays.length - i; j++) {
+                    if (arrays[j] < arrays[j + 1]) {
+                        temp = arrays[j];
+                        arrays[j] = arrays[j + 1];
+                        arrays[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < arrays.length; i++) {
+            System.out.print(arrays[i] + " ,");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arrays = {9, 3, 5, 7, 2, 1, 8};
+        BubbleSort bubbleSort = new BubbleSort(arrays);
+
+        bubbleSort.sortDESC();
+        bubbleSort.print();
+    }
 }
--- src/main/java/com/wds/algorithm/base/sort/BucketSort.java
+++ src/main/java/com/wds/algorithm/base/sort/BucketSort.java
@@ -1,7 +1,52 @@
 package com.wds.algorithm.base.sort;
 
 /**
+ * 桶排序
  * Created by wangdongsong on 2016/10/7.
  */
 public class BucketSort {
+
+    private int buckets[];
+    private int[] arrays;
+
+    public BucketSort(int bucketsLength, int[] arrays) {
+        this.buckets = new int[bucketsLength];
+        this.arrays = arrays;
+    }
+
+    /**
+     * 排序
+     */
+    public void sort() {
+        if (arrays != null && arrays.length > 1) {
+            for (int i = 0; i < arrays.length; i++) {
+                //若待排序的元素有重复，其所在下标指向的元素就+1
+                buckets[arrays[i]]++;
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = buckets.length - 1 ; i >= 0; i--) {
+            //元素中的值为几，就说明有多少个相同值的元素，就输出几篇
+            for (int j = 0; j < buckets[i]; j++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arrays = {3, 2, 5, 7, 9, 8, 6, 5};
+        BucketSort bucketSort = new BucketSort(11, arrays);
+        bucketSort.sort();
+        bucketSort.print();
+
+        System.out.println("-----------");
+
+        for (int i = 0; i < arrays.length; i ++) {
+            System.out.println(arrays[i]);
+        }
+    }
+
 }
