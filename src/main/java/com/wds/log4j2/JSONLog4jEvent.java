2f908b7a5895a4744cf61a9ac00151434099c2cd
wangdongsong <wangdongsong1229@163.com>
2016-10-10 22:18:36 +0800

    自定义的Log4jEvent&Factory

---
+++ src/main/java/com/wds/log4j2/JSONLog4jEvent.java
@@ -0,0 +1,157 @@
+package com.wds.log4j2;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.util.StdConverter;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.ThreadContext;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.impl.ThrowableProxy;
+import org.apache.logging.log4j.message.Message;
+import org.apache.logging.log4j.util.ReadOnlyStringMap;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Created by wangdongsong on 2016/10/10.
+ */
+public class JSONLog4jEvent implements LogEvent{
+    //static final String LOG_STASH_ISO8601_TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+    static final String LOG_STASH_ISO8601_TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+    static final DateFormat iso8601DateFormat = new SimpleDateFormat(LOG_STASH_ISO8601_TIMESTAMP_FORMAT);
+
+    private LogEvent wrappedLogEvent;
+
+    public JSONLog4jEvent(LogEvent wrappedLogEvent) {
+        this.wrappedLogEvent = wrappedLogEvent;
+    }
+
+    public String getVersion() {
+        return "1";//LOGSTASH VERSION
+    }
+
+    /**/
+    public String getTimestamp() {
+        return iso8601DateFormat.format(new Date(this.getTimeMillis()));
+        //return null;
+    }
+
+
+    @Override
+    public Map<String, String> getContextMap() {
+        return wrappedLogEvent.getContextMap();
+    }
+
+    @Override
+    public ReadOnlyStringMap getContextData() {
+        return null;
+    }
+
+    @Override
+    public ThreadContext.ContextStack getContextStack() {
+        return wrappedLogEvent.getContextStack();
+    }
+
+    @Override
+    public String getLoggerFqcn() {
+        return wrappedLogEvent.getLoggerFqcn();
+    }
+
+    @Override
+    public Level getLevel() {
+        return wrappedLogEvent.getLevel();
+    }
+
+    @Override
+    public String getLoggerName() {
+        return wrappedLogEvent.getLoggerName();
+    }
+
+    @Override
+    public Marker getMarker() {
+        return wrappedLogEvent.getMarker();
+    }
+
+    @Override
+    @JsonProperty("msg")
+    public Message getMessage() {
+        return wrappedLogEvent.getMessage();
+    }
+
+    @Override
+    public long getTimeMillis() {
+        return wrappedLogEvent.getTimeMillis();
+    }
+
+    @Override
+    public StackTraceElement getSource() {
+        return wrappedLogEvent.getSource();
+    }
+
+    @Override
+    public String getThreadName() {
+        return wrappedLogEvent.getThreadName();
+    }
+
+    @Override
+    public long getThreadId() {
+        return 0;
+    }
+
+    @Override
+    public int getThreadPriority() {
+        return 0;
+    }
+
+    @Override
+    public Throwable getThrown() {
+        return wrappedLogEvent.getThrown();
+    }
+
+    @Override
+    public ThrowableProxy getThrownProxy() {
+        return wrappedLogEvent.getThrownProxy();
+    }
+
+    @Override
+    public boolean isEndOfBatch() {
+        return wrappedLogEvent.isEndOfBatch();
+    }
+
+    @Override
+    public boolean isIncludeLocation() {
+        return wrappedLogEvent.isIncludeLocation();
+    }
+
+    @Override
+    public void setEndOfBatch(boolean endOfBatch) {
+        wrappedLogEvent.setEndOfBatch(endOfBatch);
+
+    }
+
+    @Override
+    public void setIncludeLocation(boolean locationRequired) {
+        wrappedLogEvent.setIncludeLocation(locationRequired);
+    }
+
+    @Override
+    public long getNanoTime() {
+        return wrappedLogEvent.getNanoTime();
+    }
+
+    /**
+     * Converter used by JsonSerilize annotation on mixin.
+     *
+     * Created by jeremyfranklin-ross on 7/28/15.
+     */
+    public static class LogEventToLogStashLogEventConverter extends StdConverter<LogEvent, JSONLog4jEvent> {
+
+        @Override
+        public JSONLog4jEvent convert(LogEvent value) {
+            return new JSONLog4jEvent(value);
+        }
+    }
+}
---
+++ src/main/java/com/wds/log4j2/JSONLog4jEventFactory.java
@@ -0,0 +1,49 @@
+package com.wds.log4j2;
+
+import com.fasterxml.jackson.databind.util.StdConverter;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.ThreadContext;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.Property;
+import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+import org.apache.logging.log4j.core.impl.LogEventFactory;
+import org.apache.logging.log4j.core.impl.ThrowableProxy;
+import org.apache.logging.log4j.message.Message;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wangdongsong on 2016/10/10.
+ */
+public class JSONLog4jEventFactory implements LogEventFactory{
+
+    private static final JSONLog4jEventFactory instance = new JSONLog4jEventFactory();
+
+    public static JSONLog4jEventFactory getInstance() {
+        return instance;
+    }
+
+    /**
+     * Creates a log event.
+     *
+     * @param loggerName The name of the Logger.
+     * @param marker An optional Marker.
+     * @param fqcn The fully qualified class name of the caller.
+     * @param level The event Level.
+     * @param data The Message.
+     * @param properties Properties to be added to the log event.
+     * @param t An optional Throwable.
+     * @return The LogEvent.
+     */
+    @Override
+    public LogEvent createEvent(final String loggerName, final Marker marker,
+                                final String fqcn, final Level level, final Message data,
+                                final List<Property> properties, final Throwable t) {
+        return new JSONLog4jEvent(new Log4jLogEvent(loggerName, marker, fqcn, level, data, properties, t));
+    }
+}
