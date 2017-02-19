ea7adb3d753da7576c1f7da21cb1d023c6e92080
wangdongsong1229 <wangdongsong1229@163.com>
2017-02-20 07:00:47 +0800

    Commit ConcurrentHashMap test code

---
+++ src/main/java/com/wds/concurrency/collection/ConcurrentHashMapTest.java
@@ -0,0 +1,15 @@
+package com.wds.concurrency.collection;
+
+/**
+ * 非阻塞队列的特别就是队列中无数据时，操作队列出现异常或返回null，不具有等待/阻塞的特点，JDK并发包中，常见的非阻塞队列有
+ * <code>ConcurrentHashMap</code>
+ * ConcurrentSkipListMap
+ * ConcurrentSkipListSet
+ * ConcurrentLinkedQueue
+ * ConcurrentLinkedDeque
+ * CopyOnWriteArrayList
+ * ConpyOnWriteArraySet
+ * Created by wangdongsong1229@163.com on 2017/2/20.
+ */
+public class ConcurrentHashMapTest {
+}
