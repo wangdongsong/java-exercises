9197c8ece0e22fd6d3588ffefa90081a5865af76
wangdongsong1229 <wangdongsong1229@163.com>
2017-02-19 21:35:00 +0800

    commit ForkJoin core method test

---
+++ src/main/java/com/wds/concurrency/forkJoin/ForkJoinCoreMethodTest.java
@@ -0,0 +1,95 @@
+package com.wds.concurrency.forkJoin;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+
+/**
+ * 核心方法练习
+ * Created by wangdongsong1229@163.com on 2017/2/19.
+ */
+public class ForkJoinCoreMethodTest {
+    private static final Logger LOGGER = LogManager.getLogger(ForkJoinCoreMethodTest.class);
+
+    public static void main(String[] args) {
+        baseExecuteTest();
+    }
+
+    private static void baseExecuteTest() {
+        ForkJoinPool pool = new ForkJoinPool();
+
+        pool.execute( () -> {
+            LOGGER.info(Thread.currentThread().getName() + Thread.currentThread().getClass());
+        });
+
+        LOGGER.info(pool.getParallelism());
+        LOGGER.info(pool.getPoolSize());
+        LOGGER.info(pool.getQueuedSubmissionCount());
+
+
+        //调用shutdown之后，任务不能再提交
+        //调用此方法后，get()不会出异常，调用shuwdownNow之后再调用 get方法会出现CancellationException异常
+        //pool.shutdown();
+
+        //execute无返回值 ，但是可以通过task.get()获取
+        ForkJoinTest.BaseForkJoinTask task = new ForkJoinTest.BaseForkJoinTask();
+        pool.execute(task);
+
+        //submit有返回值，可以通过ForkJoinTask获得返回值
+        ForkJoinTask futureTask = pool.submit(task);
+        try {
+            LOGGER.info(futureTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        //ForkJoin也可以处理Callable接口
+        futureTask = pool.submit(new Callable<String>() {
+
+            @Override
+            public String call() throws Exception {
+                return Thread.currentThread().getName() + " my is return value --Callable";
+            }
+        });
+
+        //获取并行的数量，与CPU的内核数量有关
+        LOGGER.info(pool.getParallelism());
+        //获取任务池的大小
+        LOGGER.info(pool.getPoolSize());
+        //获取已经提交但尚未被执行的任务数量
+        LOGGER.info(pool.getQueuedSubmissionCount());
+        //判断队列中是否有未执行的任务
+        LOGGER.info(pool.hasQueuedSubmissions());
+        //获得任务的总数
+        LOGGER.info(pool.getQueuedTaskCount());
+        //获得活动的线程个数
+        LOGGER.info(pool.getActiveThreadCount());
+
+
+        try {
+            LOGGER.info(futureTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Thread.sleep(1000);
+            LOGGER.info(task.get());
+            LOGGER.info(futureTask.get());
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (ExecutionException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+
+    }
+}
--- src/main/java/com/wds/concurrency/forkJoin/ForkJoinTest.java
+++ src/main/java/com/wds/concurrency/forkJoin/ForkJoinTest.java
@@ -4,9 +4,7 @@ import com.sun.prism.impl.BaseResourceFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 /**
  * Created by wangdongsong1229@163.com on 2017/2/18.
@@ -17,11 +15,91 @@ public class ForkJoinTest {
     public static void main(String[] args) {
 
         //基本用法
-        baseForJoinTest();
+        //baseForJoinActoinTest();
+
+        //baseForJoinTaskTest();
+
+        baseSumTest();
 
     }
 
-    private static void baseForJoinTest() {
+    /**
+     * 累计求和
+     */
+    private static void baseSumTest() {
+
+        ForkJoinPool pool = new ForkJoinPool();
+        SumTask task = new SumTask(1, 10);
+        pool.submit(task);
+        while (true) {
+            if (task.isDone()) {
+                pool.shutdown();
+                LOGGER.info(task.join().intValue());
+                break;
+            }
+
+        }
+    }
+
+    private static class SumTask extends RecursiveTask<Integer> {
+
+        private int beginValue;
+        private int endValue;
+
+        public SumTask(int beginValue, int endValue) {
+            this.beginValue = beginValue;
+            this.endValue = endValue;
+        }
+
+        @Override
+        protected Integer compute() {
+            if ((endValue - beginValue) != 0) {
+                int midNum = (endValue + beginValue) / 2;
+                SumTask leftTask = new SumTask(beginValue, midNum);
+                SumTask rightTask = new SumTask((midNum + 1), endValue);
+                this.invokeAll(leftTask, rightTask);
+                return leftTask.join() + rightTask.join();
+            }
+            return endValue;
+        }
+    }
+
+
+    private static void baseForJoinTaskTest() {
+
+        ForkJoinPool pool = new ForkJoinPool();
+        BaseForkJoinTask t = new BaseForkJoinTask();
+        ForkJoinTask t2 = pool.submit(t);
+
+        /*
+         * get()和join()都可以获取结果，但get()可以在主线程中做异常处理，而join()不可以，如果有异常，join抛出之后直接程序直接退出
+         */
+        try {
+            LOGGER.info(t2.hashCode() + " ---- " + t2.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        LOGGER.info(t2.hashCode() + " ---- " + t2.join());
+
+        while (!t.isDone()) {
+            pool.shutdown();
+        }
+    }
+
+    public static class BaseForkJoinTask extends RecursiveTask<String> {
+
+        @Override
+        protected String compute() {
+            LOGGER.info("BaseForkJoinTask");
+            return "BaseForkJoinTask-ReturnValue";
+        }
+    }
+
+
+    private static void baseForJoinActoinTest() {
         ForkJoinPool pool = new ForkJoinPool();
         BaseRecursiveAction task = new BaseRecursiveAction(1, 10);
         pool.submit(task);
