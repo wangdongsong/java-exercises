0d90972abcb1d621e516f57f976fd80b3fd5792b
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-29 17:05:01 +0800

    CompletionService ArrayBlockingQueue

---
+++ src/main/java/com/wds/concurrency/collection/ArrayBlockingQueueTest.java
@@ -0,0 +1,43 @@
+package com.wds.concurrency.collection;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by wangdongsong1229@163.com on 2017/7/28.
+ */
+public class ArrayBlockingQueueTest {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        BlockingQueue<Future> queue = new ArrayBlockingQueue<Future>(10);
+        Executor executor = Executors.newFixedThreadPool(3);
+        CompletionService cs = new ExecutorCompletionService(executor, queue);
+
+        for (int i = 10; i > 1; i--) {
+            CallTak task = new CallTak(new Long(i));
+            Future future = cs.submit(task);
+            String result = (String)future.get();
+            queue.take();
+            System.out.println(result);
+        }
+
+        System.out.println(queue.size());
+
+    }
+
+    static class CallTak implements Callable<String>{
+
+        private long sleepTime;
+
+        public CallTak(long sleepTime) {
+            this.sleepTime = sleepTime;
+        }
+
+        @Override
+        public String call() throws Exception {
+            Thread.sleep(sleepTime);
+
+            return "result-" + sleepTime;
+        }
+    }
+
+}
