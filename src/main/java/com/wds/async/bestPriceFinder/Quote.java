c83affb46b074584a7cb0fbb6b3119de6f6e0222
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-30 13:55:52 +0800

    折扣

---
+++ src/main/java/com/wds/async/bestPriceFinder/Quote.java
@@ -0,0 +1,38 @@
+package com.wds.async.bestPriceFinder;
+
+import org.apache.hadoop.io.DoubleWritable;
+
+/**
+ * Created by wangdongsong1229@163.com on 2017/7/30.
+ */
+public class Quote {
+    private final String shopName;
+    private final double price;
+    private final Discount.Code discountCode;
+
+    public Quote(String shopName, double price, Discount.Code discountCode) {
+        this.shopName = shopName;
+        this.price = price;
+        this.discountCode = discountCode;
+    }
+
+    public static Quote parse(String s){
+        String[] split = s.split(":");
+        String shopName = split[0];
+        double price = Double.parseDouble(split[1]);
+        Discount.Code discountCode = Discount.Code.valueOf(split[2]);
+        return new Quote(shopName, price, discountCode);
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Discount.Code getDiscountCode() {
+        return discountCode;
+    }
+}
