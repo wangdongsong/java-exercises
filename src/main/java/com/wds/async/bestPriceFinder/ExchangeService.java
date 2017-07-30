5906ab837787d958ef5741ea0cf1e0ee044e8f93
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-30 14:32:43 +0800

    汇率

---
+++ src/main/java/com/wds/async/bestPriceFinder/ExchangeService.java
@@ -0,0 +1,28 @@
+package com.wds.async.bestPriceFinder;
+
+import static com.wds.async.bestPriceFinder.Util.delay;
+
+/**
+ * Created by wangdongsong1229@163.com on 2017/7/30.
+ */
+public class ExchangeService {
+
+    public enum Money {
+        USD(1.0), EUR(1.35387), GBP(1.69715), CAD(.92106), MXN(.07683);
+
+        private final double rate;
+
+        Money(double rate) {
+            this.rate = rate;
+        }
+    }
+
+    public static double getRate(Money source, Money destination) {
+        return getRateWithDelay(source, destination);
+    }
+
+    private static double getRateWithDelay(Money source, Money destination) {
+        delay();
+        return destination.rate / source.rate;
+    }
+}
