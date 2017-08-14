ba9079839ba1b2eb8f21cc0fe338538ed847d499
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-15 06:24:07 +0800

    update java8 lambda

--- src/main/java/com/wds/java8InAction/ch01/Test.java
+++ src/main/java/com/wds/java8InAction/ch01/Test.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.mapreduce.v2.app.webapp.App;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Created by wangdongsong1229@163.com on 2017/7/30.
@@ -11,7 +12,27 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
+        //Java8
+        filterApples(createApple(), Test::isGreenApple);
+    }
 
+    //Java8写法
+    public static boolean isGreenApple(Apple apple) {
+        return "green".equalsIgnoreCase(apple.getColor());
+    }
+
+    public static boolean isHeavyApple(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+        List<Apple> result = new ArrayList<>();
+        inventory.forEach( apple -> {
+           if(p.test(apple)){
+               result.add(apple);
+           }
+        });
+        return result;
     }
 
 
@@ -45,7 +66,7 @@ public class Test {
         return result;
     }
 
-    public static List<Apple> createAppl(){
+    public static List<Apple> createApple(){
         List<Apple> list = new ArrayList<>();
         list.add(new Apple("green", 155));
         list.add(new Apple("red", 145));
