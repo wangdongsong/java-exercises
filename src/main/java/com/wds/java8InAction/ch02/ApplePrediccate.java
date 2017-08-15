5c78e1bd959e79bdcac207379ade7c25a8c9ce95
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-16 06:53:15 +0800

    自定义Predicated接口

---
+++ src/main/java/com/wds/java8InAction/ch02/ApplePrediccate.java
@@ -0,0 +1,10 @@
+package com.wds.java8InAction.ch02;
+
+import com.wds.java8InAction.ch01.Apple;
+
+/**
+ * Created by wangdongsong1229@163.com on 2017/8/15.
+ */
+public interface ApplePrediccate {
+    boolean test(Apple apple);
+}
