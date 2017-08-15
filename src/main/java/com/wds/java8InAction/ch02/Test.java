bf8f89bf922361c50bb1974ca91af0f7439fec11
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-16 06:52:54 +0800

    main class

---
+++ src/main/java/com/wds/java8InAction/ch02/Test.java
@@ -0,0 +1,12 @@
+package com.wds.java8InAction.ch02;
+
+/**
+ * Created by wangdongsong1229@163.com on 2017/8/15.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
