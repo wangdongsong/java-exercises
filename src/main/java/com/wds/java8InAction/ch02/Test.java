355955ee64e69f241e76bf640bd2d84ed975d8e1
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-17 06:34:36 +0800

    update

--- src/main/java/com/wds/java8InAction/ch02/Test.java
+++ src/main/java/com/wds/java8InAction/ch02/Test.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.mapreduce.v2.app.webapp.App;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Created by wangdongsong1229@163.com on 2017/8/15.
@@ -14,6 +15,22 @@ public class Test {
     public static void main(String[] args) {
         List<Apple> greenApple = filterApples(createApple(), new AppleColorPredicate());
         greenApple.forEach((apple) -> System.out.println(apple));
+
+        //java8
+        filter(createApple(), (Apple apple) -> "red".equalsIgnoreCase(apple.getColor()));
+
+        //排序
+        createApple().sort((Apple a1, Apple a2) -> a1.getColor().compareTo(a2.getColor()));
+    }
+
+    public static <T> List<T> filter(List<T> inventory, Predicate<T> p) {
+        List<T> result = new ArrayList<T>();
+        for (T e : inventory) {
+            if (p.test(e)) {
+                result.add(e);
+            }
+        }
+        return result;
     }
 
     public static List<Apple> filterApples(List<Apple> inventory, ApplePrediccate p) {
