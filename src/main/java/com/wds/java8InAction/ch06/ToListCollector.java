7001ddc8f31e0b23964f53240625279d7f4f9450
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-23 21:35:11 +0800

    自定义的toList收集器：add doc

--- src/main/java/com/wds/java8InAction/ch06/ToListCollector.java
+++ src/main/java/com/wds/java8InAction/ch06/ToListCollector.java
@@ -14,12 +14,20 @@ import java.util.stream.Collector;
  */
 public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
 
+    /**
+     * 建立新的结果容器：
+     * @return
+     */
     @Override
     public Supplier supplier() {
         //return ArrayList::new;
         return () -> new ArrayList<T>();
     }
 
+    /**
+     * 将元素添加到结果容器，该方法有两个参数：（1）保存归约结果的累加器（已收集了流中的前n-1项目），（2）第n个元素本身
+     * @return
+     */
     @Override
     public BiConsumer<List<T>, T> accumulator() {
         //两者等同
@@ -27,6 +35,11 @@ public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
         return (list, item) -> list.add(item);
     }
 
+    /**
+     * 合并两个结果容器：该方法返回一个供归约操作使用的函数，定义对流的各个子部分进行并行处理时，各个子部分归约所得的累加器
+     * 要如何合并
+     * @return
+     */
     @Override
     public BinaryOperator<List<T>> combiner() {
         return (list1, list2) -> {
@@ -35,13 +48,25 @@ public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
         };
     }
 
+    /**
+     * 对结果容器应用最终转换：在遍历完流后，finisher方法必须返回在累积过程的最后要调用的一个函数，以便
+     * 将累加器对象转换为整个集合操作的最终结果。
+     * @return
+     */
     @Override
     public Function<List<T>, List<T>> finisher() {
         return Function.identity();
     }
 
+    /**
+     * 定义收集器行为
+     * @return
+     */
     @Override
     public Set<Characteristics> characteristics() {
+        //Characteristics.UNORDERED; 归约结果不受流中项目遍历和累积顺序的影响
+        //Characteristics.CONCURRENT; accumulator函数可以从多个线程同时调用，且该收集器可以并行归约流
+        //Characteristics.IDENTITY_FINISH; 表明完成器方法返回的函数是一个恒等函数。
         return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH, Characteristics.CONCURRENT));
     }
 }
