2281708e9e94a28d6b3bd5e8208f1c3660ebfd84
wangdongsong <wangdongsong1229@163.com>
2018-01-01 19:55:42 +0800

    Tomcat-Response

---
+++ src/main/java/com/wds/tomcat/ex01/Response.java
@@ -0,0 +1,53 @@
+package com.wds.tomcat.ex01;
+
+import java.io.*;
+
+/**
+ * Created by wangdongsong1229@163.com on 2017/12/27.
+ */
+public class Response {
+    private static final int BUFFER_SIZE = 1024;
+    Request request;
+    OutputStream output;
+
+    public Response(OutputStream output) {
+        this.output = output;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public void sendStaticResource() throws IOException {
+        byte[] bytes = new byte[BUFFER_SIZE];
+        FileInputStream fis = null;
+
+        try {
+            System.out.println(HttpServer.WEB_ROOT);
+            File file = new File(HttpServer.WEB_ROOT, request.getUri());
+            if (file.exists()) {
+                fis = new FileInputStream(file);
+                int ch = fis.read(bytes, 0, BUFFER_SIZE);
+                while (ch != -1) {
+                    output.write(bytes, 0, ch);
+                    ch = fis.read(bytes, 0, BUFFER_SIZE);
+                }
+            } else {
+                String errorMessage = "HTTP/1.1 404 File not Found\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: 23\r\n" +
+                        "\r\n" +
+                        "<h1>File not found</h1>";
+                output.write(errorMessage.getBytes());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (fis != null) {
+                fis.close();
+            }
+        }
+    }
+}
