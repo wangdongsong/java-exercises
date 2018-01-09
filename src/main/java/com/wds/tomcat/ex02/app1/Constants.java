30dc499a0399725f7016f1c0673a21738a85e60f
wangdongsong <wangdongsong1229@163.com>
2018-01-09 21:43:35 +0800

    tomcat-一个简单的Servlet容器：常量类

---
+++ src/main/java/com/wds/tomcat/ex02/app1/Constants.java
@@ -0,0 +1,11 @@
+package com.wds.tomcat.ex02.app1;
+
+import java.io.File;
+
+/**
+ * Created by wangdongsong1229@163.com on 2018/1/9.
+ */
+public class Constants {
+
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+}
