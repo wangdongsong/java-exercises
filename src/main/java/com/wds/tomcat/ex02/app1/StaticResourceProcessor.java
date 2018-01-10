480293fc2bd10deafde3031c68ac8f387280965e
wangdongsong <wangdongsong1229@163.com>
2018-01-10 21:12:08 +0800

    tomcat-一个简单的Servlet容器：updated

--- src/main/java/com/wds/tomcat/ex02/app1/StaticResourceProcessor.java
+++ src/main/java/com/wds/tomcat/ex02/app1/StaticResourceProcessor.java
@@ -1,10 +1,17 @@
 package com.wds.tomcat.ex02.app1;
 
+import java.io.IOException;
+
 /**
  * Created by wangdongsong1229@163.com on 2018/1/9.
  */
 public class StaticResourceProcessor {
-    public void process(Request request, Response response) {
 
+    public void process(Request request, Response response) {
+        try {
+            response.sendStaticResource();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
