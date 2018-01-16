88efde4dff6e2e4ec4f9aaf4a6f9b20934e3a269
wangdongsong <wangdongsong1229@163.com>
2018-01-16 22:48:22 +0800

    tomcat-一个简单的Servlet容器：ResponseFacade

---
+++ src/main/java/com/wds/tomcat/ex02/app2/ResponseFacade.java
@@ -0,0 +1,93 @@
+package com.wds.tomcat.ex02.app2;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+/**
+ * Created by wangdongsong1229@163.com on 2018/1/14.
+ */
+public class ResponseFacade implements ServletResponse {
+    private ServletResponse response = null;
+
+    public ResponseFacade(ServletResponse response) {
+        this.response = response;
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return this.response.getCharacterEncoding();
+    }
+
+    @Override
+    public String getContentType() {
+        return this.response.getContentType();
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        return response.getOutputStream();
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        return this.response.getWriter();
+    }
+
+    @Override
+    public void setCharacterEncoding(String charset) {
+        this.response.setCharacterEncoding(charset);
+    }
+
+    @Override
+    public void setContentLength(int len) {
+        this.response.setContentLength(len);
+    }
+
+    @Override
+    public void setContentType(String type) {
+        this.response.setContentType(type);
+    }
+
+    @Override
+    public void setBufferSize(int size) {
+        this.response.setBufferSize(size);
+    }
+
+    @Override
+    public int getBufferSize() {
+        return this.response.getBufferSize();
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+        this.response.flushBuffer();
+    }
+
+    @Override
+    public void resetBuffer() {
+        this.response.resetBuffer();
+    }
+
+    @Override
+    public boolean isCommitted() {
+        return this.response.isCommitted();
+    }
+
+    @Override
+    public void reset() {
+        this.response.reset();
+    }
+
+    @Override
+    public void setLocale(Locale loc) {
+        this.response.setLocale(loc);
+    }
+
+    @Override
+    public Locale getLocale() {
+        return this.response.getLocale();
+    }
+}
