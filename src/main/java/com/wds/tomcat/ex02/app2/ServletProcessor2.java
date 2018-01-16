393eba2342e68bac73be8c3200d3c71fad3a5326
wangdongsong <wangdongsong1229@163.com>
2018-01-16 22:48:46 +0800

    tomcat-一个简单的Servlet容器：ServerProcess使用Facade方式

---
+++ src/main/java/com/wds/tomcat/ex02/app2/ServletProcessor2.java
@@ -0,0 +1,66 @@
+package com.wds.tomcat.ex02.app2;
+
+import com.wds.tomcat.ex02.app1.Constants;
+import com.wds.tomcat.ex02.app1.Request;
+import com.wds.tomcat.ex02.app1.Response;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandler;
+
+/**
+ * Created by wangdongsong1229@163.com on 2018/1/9.
+ */
+public class ServletProcessor2 {
+
+    public void process(Request request, Response response) {
+        String uri = request.getUri();
+        String servletName = uri.substring(uri.lastIndexOf("/") + 1);
+        URLClassLoader loader = null;
+
+        try {
+            //创建类装载器
+            URL[] urls = new URL[1];
+            URLStreamHandler streamHandler = null;
+            File classPath = new File(Constants.WEB_ROOT);
+            //从org.apache.catalina.startup.ClassLoaderFactory中学习创建资源仓库
+            String repository = (new URL("file", null, classPath.getCanonicalPath() + File.separator)).toString();
+            System.out.println(repository);
+            urls[0] = new URL(null, repository, streamHandler);
+            //创建类加载器
+            loader = new URLClassLoader(urls);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Class myClass = null;
+        try {
+            //加载Servlet
+            myClass = loader.loadClass(servletName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Servlet servlet = null;
+            RequestFacade requestFacade = new RequestFacade(request);
+            ResponseFacade responseFacade = new ResponseFacade(response);
+            servlet = (Servlet) myClass.newInstance();
+            servlet.service((ServletRequest) requestFacade, (ServletResponse) responseFacade);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
