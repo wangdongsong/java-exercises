9823f062b4dfe11d80a9d118f0ab9b46b84821be
wangdongsong <wangdongsong1229@163.com>
2018-01-21 11:24:47 +0800

    tomcat连接器-updated

--- src/main/java/com/wds/tomcat/ex03/utils/RequestUtil.java
+++ src/main/java/com/wds/tomcat/ex03/utils/RequestUtil.java
@@ -1,7 +1,9 @@
 package com.wds.tomcat.ex03.utils;
 
 import javax.servlet.http.Cookie;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Tomcat新的版本源码中已无parseCookieHeader方法
@@ -36,11 +38,104 @@ public class RequestUtil {
                     cookies.add(new Cookie(name, value));
                 }
             } catch (Throwable e) {
-                ;
+                e.printStackTrace();
             }
         }
 
         return ((Cookie[]) cookies.toArray(new Cookie[cookies.size()]));
 
+    }
+
+    public static void parseParameters(Map map, String data, String encoding)
+            throws UnsupportedEncodingException {
+
+        if ((data != null) && (data.length() > 0)) {
+
+            // use the specified encoding to extract bytes out of the
+            // given string so that the encoding is not lost. If an
+            // encoding is not specified, let it use platform default
+            byte[] bytes = null;
+            try {
+                if (encoding == null) {
+                    bytes = data.getBytes();
+                } else {
+                    bytes = data.getBytes(encoding);
+                }
+            } catch (UnsupportedEncodingException uee) {
+                uee.printStackTrace();
+            }
+
+            parseParameters(map, bytes, encoding);
+        }
+
+    }
+
+    public static void parseParameters(Map map, byte[] data, String encoding)
+            throws UnsupportedEncodingException {
+
+        if (data != null && data.length > 0) {
+            int    ix = 0;
+            int    ox = 0;
+            String key = null;
+            String value;
+            while (ix < data.length) {
+                byte c = data[ix++];
+                switch ((char) c) {
+                    case '&':
+                        value = new String(data, 0, ox, encoding);
+                        if (key != null) {
+                            putMapEntry(map, key, value);
+                            key = null;
+                        }
+                        ox = 0;
+                        break;
+                    case '=':
+                        if (key == null) {
+                            key = new String(data, 0, ox, encoding);
+                            ox = 0;
+                        } else {
+                            data[ox++] = c;
+                        }
+                        break;
+                    case '+':
+                        data[ox++] = (byte)' ';
+                        break;
+                    case '%':
+                        data[ox++] = (byte)((convertHexDigit(data[ix++]) << 4)
+                                + convertHexDigit(data[ix++]));
+                        break;
+                    default:
+                        data[ox++] = c;
+                }
+            }
+            //The last value does not end in '&'.  So save it now.
+            if (key != null) {
+                value = new String(data, 0, ox, encoding);
+                putMapEntry(map, key, value);
+            }
+        }
+
+    }
+
+    private static byte convertHexDigit( byte b ) {
+        if ((b >= '0') && (b <= '9')) return (byte)(b - '0');
+        if ((b >= 'a') && (b <= 'f')) return (byte)(b - 'a' + 10);
+        if ((b >= 'A') && (b <= 'F')) return (byte)(b - 'A' + 10);
+        return 0;
+    }
+
+
+    private static void putMapEntry( Map map, String name, String value) {
+        String[] newValues;
+        String[] oldValues = (String[]) map.get(name);
+        if (oldValues == null) {
+            newValues = new String[1];
+            newValues[0] = value;
+        } else {
+            newValues = new String[oldValues.length + 1];
+            System.arraycopy(oldValues, 0, newValues, 0, oldValues.length);
+            newValues[oldValues.length] = value;
+        }
+        map.put(name, newValues);
     }
 }
