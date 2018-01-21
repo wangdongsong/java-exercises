90816db3c89bc9ecda49870e30deffc9210ede18
wangdongsong <wangdongsong1229@163.com>
2018-01-21 11:25:42 +0800

    tomcat连接器-默认连接器

---
+++ src/main/java/com/wds/tomcat/ex03/connector/http/HttpProcessor.java
@@ -0,0 +1,258 @@
+package com.wds.tomcat.ex03.connector.http;
+
+import com.wds.tomcat.ex03.ServletProcessor;
+import com.wds.tomcat.ex03.utils.RequestUtil;
+import org.apache.naming.StringManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Created by wangdongsong1229@163.com on 2018/1/17.
+ */
+public class HttpProcessor {
+
+    private HttpConnector connector;
+    private HttpRequest request;
+    private HttpResponse response;
+    private HttpRequestLine requestLine = new HttpRequestLine();
+    protected StringManager sm =StringManager.getManager("com.wds.tomcat.ex03.connector.http");
+//    protected StringManager
+
+    public HttpProcessor(HttpConnector connector) {
+        this.connector = connector;
+    }
+
+    /**
+     * 接收来自传入的HTTP请求的套接字，对每个传入的HTTP请求完成以下4个操作：
+     *  创建一个HttpRequest对象
+     *  创建一个HttpResponse对象
+     *  解析HTTP请求的第1行内容和请求头信息，填充HttpRequest对象
+     *  将HttpRequest对象和HttpResponse对象传递给ServletProcessor或StaticResourceProcessor的process方法
+     * @param socket
+     */
+    public void process(Socket socket) {
+        SocketInputStream input = null;
+        OutputStream output = null;
+
+        try {
+            input = new SocketInputStream(socket.getInputStream(), 2048);
+            output = socket.getOutputStream();
+
+            request = new HttpRequest(input);
+            response = new HttpResponse(output);
+            response.setRequest(request);
+
+            response.setHeader("Server", "com.wds.tomcat");
+            parseRequest(input, output);
+            parseHeaders(input);
+
+            if (request.getRequestURI().startsWith("/servlet/")) {
+                ServletProcessor processor = new ServletProcessor();
+                processor.process(request, response);
+            } else {
+                StaticResourceProcessor processor = new StaticResourceProcessor();
+                processor.process(request, response);
+            }
+
+            //关闭连接但不关闭应用
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 解析请求头
+     * @param input
+     */
+    private void parseHeaders(SocketInputStream input) throws ServletException {
+        while (true) {
+            HttpHeader header = new HttpHeader();
+            input.readHeader(header);
+
+            if (header.nameEnd == 0) {
+                if (header.valueEnd == 0) {
+                    return;
+                } else {
+                    throw new ServletException(sm.getString("httpProcessor.parseHeaders.colon"));
+                }
+            }
+
+            String name = new String(header.name, 0, header.nameEnd);
+            String value = new String(header.value, 0, header.valueEnd);
+
+            request.addHeader(name, value);
+
+            if (name.equals("cookie")) {
+                Cookie cookies[] = RequestUtil.parseCookieHeader(value);
+                for (int i = 0; i < cookies.length; i++) {
+                    if (cookies[i].getName().equals("jsessionid")) {
+                        // Override anything requested in the URL
+                        if (!request.isRequestedSessionIdFromCookie()) {
+                            // Accept only the first session id cookie
+                            request.setRequestedSessionId(cookies[i].getValue());
+                            request.setRequestedSessionCookie(true);
+                            request.setRequestedSessionURL(false);
+                        }
+                    }
+                    request.addCookie(cookies[i]);
+                }
+            } else if (name.equals("content-length")) {
+                int n = -1;
+                try {
+                    n = Integer.parseInt(value);
+                } catch (Exception e) {
+                    throw new ServletException(sm.getString("httpProcessor.parseHeaders.contentLength"));
+                }
+                request.setContentLength(n);
+            } else if (name.equals("content-type")) {
+                request.setContentType(value);
+            }
+        }
+    }
+
+    private void parseRequest(SocketInputStream input, OutputStream output) throws ServletException {
+        input.readRequestLine(requestLine);
+        String method = new String(requestLine.method, 0, requestLine.methodEnd);
+
+        String uri = null;
+        String protocol = new String(requestLine.protocol, 0, requestLine.protocolEnd);
+
+        if (method.length() < 1) {
+            throw new ServletException("Missing HTTP request method");
+        } else if (requestLine.uriEnd < 1) {
+            throw new ServletException("Missing HTTP request URI");
+        }
+
+        int question = requestLine.indexOf("?");
+        if (question >= 0) {
+            request.setQueryString(new String(requestLine.uri, question + 1, requestLine.uriEnd - question - 1));
+            uri = new String(requestLine.uri, 0, question);
+        } else {
+            request.setQueryString(null);
+            uri = new String(requestLine.uri, 0, requestLine.uriEnd);
+        }
+
+        if (!uri.startsWith("/")) {
+            int pos = uri.indexOf("://");
+            if (pos != -1) {
+                pos = uri.indexOf("/", pos + 3);
+                if (pos == -1) {
+                    uri = "";
+                } else {
+                    uri = uri.substring(pos);
+                }
+            }
+        }
+
+        String match = ";jsessionid=";
+        int semicolon = uri.indexOf(match);
+        if (semicolon >= 0) {
+            String rest = uri.substring(semicolon + match.length());
+            int semicolon2 = rest.indexOf(";");
+            if (semicolon2 >= 0) {
+                request.setRequestedSessionId(rest.substring(0, semicolon2));
+            } else {
+                request.setRequestedSessionId(rest);
+                rest = "";
+            }
+            request.setRequestedSessionURL(true);
+            uri = uri.substring(0, semicolon) + rest;
+        } else {
+            request.setRequestedSessionId(null);
+            request.setRequestedSessionURL(false);
+        }
+
+        String normalizedUri = normalize(uri);
+        ((HttpRequest) request).setMethod(method);
+        request.setProtocol(protocol);
+        if (normalizedUri != null) {
+            ((HttpRequest) request).setRequestURI(normalizedUri);
+        } else {
+            ((HttpRequest) request).setRequestURI(uri);
+        }
+
+        if (normalizedUri == null) {
+            throw new ServletException("Invalid URI: '" + uri + "'");
+        }
+
+    }
+
+    protected String normalize(String path) {
+        if (path == null)
+            return null;
+        // Create a place for the normalized path
+        String normalized = path;
+
+        // Normalize "/%7E" and "/%7e" at the beginning to "/~"
+        if (normalized.startsWith("/%7E") || normalized.startsWith("/%7e"))
+            normalized = "/~" + normalized.substring(4);
+
+        // Prevent encoding '%', '/', '.' and '\', which are special reserved
+        // characters
+        if ((normalized.indexOf("%25") >= 0)
+                || (normalized.indexOf("%2F") >= 0)
+                || (normalized.indexOf("%2E") >= 0)
+                || (normalized.indexOf("%5C") >= 0)
+                || (normalized.indexOf("%2f") >= 0)
+                || (normalized.indexOf("%2e") >= 0)
+                || (normalized.indexOf("%5c") >= 0)) {
+            return null;
+        }
+
+        if (normalized.equals("/."))
+            return "/";
+
+        // Normalize the slashes and add leading slash if necessary
+        if (normalized.indexOf('\\') >= 0)
+            normalized = normalized.replace('\\', '/');
+        if (!normalized.startsWith("/"))
+            normalized = "/" + normalized;
+
+        // Resolve occurrences of "//" in the normalized path
+        while (true) {
+            int index = normalized.indexOf("//");
+            if (index < 0)
+                break;
+            normalized = normalized.substring(0, index) +
+                    normalized.substring(index + 1);
+        }
+
+        // Resolve occurrences of "/./" in the normalized path
+        while (true) {
+            int index = normalized.indexOf("/./");
+            if (index < 0)
+                break;
+            normalized = normalized.substring(0, index) +
+                    normalized.substring(index + 2);
+        }
+
+        // Resolve occurrences of "/../" in the normalized path
+        while (true) {
+            int index = normalized.indexOf("/../");
+            if (index < 0)
+                break;
+            if (index == 0)
+                return (null);  // Trying to go outside our context
+            int index2 = normalized.lastIndexOf('/', index - 1);
+            normalized = normalized.substring(0, index2) +
+                    normalized.substring(index + 3);
+        }
+
+        // Declare occurrences of "/..." (three or more dots) to be invalid
+        // (on some Windows platforms this walks the directory tree!!!)
+        if (normalized.indexOf("/...") >= 0)
+            return (null);
+
+        // Return the normalized path that we have completed
+        return (normalized);
+
+    }
+
+}
